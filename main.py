c6a3938aa82c6d690d99c61a0f3d2e63dc31e22d
Michał Wójcik <mwooycik@gmail.com>
2024-05-18 16:30:31 +0200

    works on rotation animation and logic

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ VEL = 5  # pixels or degrees
 
 class AMR():
     def __init__(self, x=WIDTH // 2, y=HEIGHT // 2):
-        self.width, self.height = 50, 50
+        self.width, self.height = 50, 75
         self.x, self.y = WIDTH // 2, HEIGHT // 2
-        self.angle = 0  # angle > 0 => rotated left
+        self.angle_rad = 0  # angle > 0 => rotated left
         self.color = WHITE
         self.lin_speed = VEL
-        self.rot_speed = VEL
+        self.rot_speed_deg = 0.5
 
     def handle_movement(self, keys):
         if keys[pygame.K_UP]:
@@ -36,18 +36,39 @@ class AMR():
             self.rotate(-1)
 
     def move_fwd_bwd(self, direction):
-        self.y += self.lin_speed * math.cos(self.angle) * direction
-        self.x += self.lin_speed * math.sin(self.angle) * direction
+        self.y += self.lin_speed * math.cos(self.angle_rad) * direction
+        self.x += self.lin_speed * math.sin(self.angle_rad) * direction
         print(f"self.x = {self.x}, self.y = {self.y}")
 
     def rotate(self, direction):
-        self.angle += self.rot_speed * 180 / math.pi * direction
+        self.angle_rad += self.rot_speed_deg * math.pi / 180 * direction
+        print(f"current angle: {self.angle_rad / 180 * math.pi} [deg], {self.angle_rad} [rad]")
 
     def draw(self):
         WIN.fill(BLACK)
-        rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        pygame.draw.rect(WIN, self.color, rect)
+        # rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        # pygame.draw.rect(WIN, self.color, rect)
+        self.draw_rectangle()
         pygame.display.update()
+
+    def draw_rectangle(self):
+        points = []
+        half_diag = math.sqrt((self.height / 2) ** 2 + (self.width / 2) ** 2)
+
+        rect_ang = math.atan2(self.height / 2, self.width / 2)  # from x-axis perspective
+
+        rect_angles = [rect_ang, -rect_ang + math.pi, rect_ang + math.pi, -rect_ang]  # angle to each corner
+
+        # Convert rotation from degrees to radians.
+        rot_rad = (math.pi / 180) * self.angle_rad
+
+        # Calculate the coordinates of each point.
+        for angle in rect_angles:
+            y_offset = -1 * half_diag * math.sin(angle + rot_rad)
+            x_offset = half_diag * math.cos(angle + rot_rad)
+            points.append((self.x + x_offset, self.y + y_offset))
+
+        pygame.draw.polygon(WIN, self.color, points)
 
 
 def main():
