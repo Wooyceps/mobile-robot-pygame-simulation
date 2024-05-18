fbcca8c9458d5f03765e52d66d7bb55b22434ca1
Michał Wójcik <mwooycik@gmail.com>
2024-05-18 18:05:20 +0200

    movement enhanced to match with keyboard arrows and front light feature added

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+AIUT_BLUE = (0, 149, 218)
 
 FPS = 60
-VEL = 5  # pixels or degrees
 
 
 class AMR():
@@ -20,16 +20,17 @@ class AMR():
         self.width, self.height = 50, 75
         self.x, self.y = WIDTH // 2, HEIGHT // 2
         self.angle_rad = 0  # angle > 0 => rotated left
-        self.color = WHITE
-        self.lin_speed = VEL
-        self.rot_speed_deg = 0.5
+        self.color = BLACK
+        self.front_color = AIUT_BLUE
+        self.lin_speed = 3 # pixels per frame
+        self.rot_speed_deg = 1  # degree per frame
 
     def handle_movement(self, keys):
         if keys[pygame.K_UP]:
             print("K_UP")
-            self.move_fwd_bwd(1)
-        if keys[pygame.K_DOWN]:
             self.move_fwd_bwd(-1)
+        if keys[pygame.K_DOWN]:
+            self.move_fwd_bwd(1)
         if keys[pygame.K_LEFT]:
             self.rotate(1)
         if keys[pygame.K_RIGHT]:
@@ -44,28 +45,44 @@ class AMR():
         self.angle_rad += self.rot_speed_deg * math.pi / 180 * direction
         print(f"current angle: {self.angle_rad * 180 / math.pi} [deg], {self.angle_rad} [rad]")
 
-    def draw(self):
-        WIN.fill(BLACK)
-        # rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        # pygame.draw.rect(WIN, self.color, rect)
-        self.draw_rectangle()
-        pygame.display.update()
+    def draw_AMR(self):
+        WIN.fill(WHITE)
 
-    def draw_rectangle(self):
+        # AMR body base
         points = []
+
         half_diag = math.sqrt((self.height / 2) ** 2 + (self.width / 2) ** 2)
-
         rect_ang = math.atan2(self.height / 2, self.width / 2)  # from x-axis perspective
-
         rect_angles = [rect_ang, -rect_ang + math.pi, rect_ang + math.pi, -rect_ang]  # angle to each corner
 
-        # Calculate the coordinates of each point.
         for angle in rect_angles:
             y_offset = -1 * half_diag * math.sin(angle + self.angle_rad)
             x_offset = half_diag * math.cos(angle + self.angle_rad)
             points.append((self.x + x_offset, self.y + y_offset))
 
         pygame.draw.polygon(WIN, self.color, points)
+
+        # AMR front light feature
+        points = []
+
+        half_diag_outer = math.sqrt((self.height / 2) ** 2 + (self.width / 2) ** 2)
+        half_diag_inner = math.sqrt((self.height / 2 - 5) ** 2 + (self.width / 2) ** 2)
+        rect_ang_outer = math.atan2(self.height / 2, self.width / 2)  # from x-axis perspective
+        rect_ang_inner = math.atan2(self.height / 2 - 5, self.width / 2)  # from x-axis perspective
+        rect_angles = [rect_ang_outer, -rect_ang_outer + math.pi, -rect_ang_inner + math.pi, rect_ang_inner]  # angle to each corner
+
+        for idx, angle in enumerate(rect_angles):
+            if idx < 2:
+                y_offset = -1 * half_diag_outer * math.sin(angle + self.angle_rad)
+                x_offset = half_diag_outer * math.cos(angle + self.angle_rad)
+            else:
+                y_offset = -1 * half_diag_inner * math.sin(angle + self.angle_rad)
+                x_offset = half_diag_inner * math.cos(angle + self.angle_rad)
+            points.append((self.x + x_offset, self.y + y_offset))
+
+        pygame.draw.polygon(WIN, self.front_color, points)
+
+        pygame.display.update()
 
 
 def main():
@@ -84,11 +101,8 @@ def main():
 
         keys = pygame.key.get_pressed()
         amr.handle_movement(keys)
-        amr.draw()
+        amr.draw_AMR()
 
 
 if __name__ == "__main__":
     main()
-
-# rotated_amr = pygame.transform.rotate(amr, amr.angle)
-# WIN.blit(rotated_amr, amr)
